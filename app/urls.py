a7aa43380803a28752d330af497b61abf71dfc60
Ajai Danial <ajaidanial@gmail.com>
2019-05-15 15:02:45 +0530

    dev start | 6 | server side validation start | 2

--- app/urls.py
+++ app/urls.py
@@ -1,9 +1,10 @@
-from django.urls import path
 from django.conf import settings
 from django.conf.urls.static import static
+from django.urls import path
 
 from . import views
 
 urlpatterns = [
+    path('validate', views.validate, name='validate'),
     path('', views.login, name='login'),
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
--- app/views.py
+++ app/views.py
@@ -1,8 +1,15 @@
 from django.http import HttpResponse
 from django.template import loader
+from django.views.decorators.csrf import csrf_exempt
 
 
 def login(request):
     template = loader.get_template('login.html')
     context = {}
     return HttpResponse(template.render(context, request))
+
+
+@csrf_exempt
+def validate(request):
+    if request.method == 'POST':
+        return HttpResponse("Hello World")
