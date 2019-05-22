b1043ca8738fae28a59d3b8a89876a62e2976ba3
Ajai Danial <ajaidanial@gmail.com>
2019-05-22 10:09:46 +0530

    form | test 2

--- app/static/js/main.js
+++ app/static/js/main.js
@@ -6,20 +6,15 @@
         // Loop over them and prevent submission
         var validation = Array.prototype.filter.call(forms, function (form) {
             form.addEventListener('submit', function (event) {
-                if (form.checkValidity() === false) {
-                    event.preventDefault();
-                    event.stopPropagation();
-                }
+                event.preventDefault();
+                event.stopPropagation();
                 if (form.checkValidity() === true) {
-                    if (form.attr('id') === 'login') {
-                        // login form
-
+                    if (form.id === 'login') {
+                        ajaxReq($('#login'))
                     }
                     else {
-                        // sign up form
-
+                        ajaxReq($('#signup'))
                     }
-                    alert("test");
                 }
                 form.classList.add('was-validated');
             }, false);
@@ -35,41 +30,34 @@ $('#signin_un').on('input', function (e) {
     $('#signin_un').val(this.value.replace(" ", "").replace(/[^a-zA-Z0-9]/g, ''));
 });
 
-function showLoader(btn) {
-    btn.prop('disabled', true);
-    btn.find('.spinner-border').removeClass("d-none")
+function show_hide_Loader(form) {
+    var btn = form.find('.submit_btn');
+    if (btn.prop('disables')) {
+        btn.prop('disabled', false);
+        btn.find('.spinner-border').addClass("d-none")
+    }
+    else {
+        btn.prop('disabled', true);
+        btn.find('.spinner-border').removeClass("d-none")
+    }
 }
 
-showLoader($('#login').find('.submit_btn'));
-
-// // TEST
-// $('#contactForm').validate({
-//     submitHandler: function (form) {
-//         // var sLoader = $('#submit-loader');
-//         $.ajax({
-//             type: "POST",
-//             url: "inc/sendEmail.php",
-//             data: $(form).serialize(),
-//             // beforeSend: function () {
-//             //     sLoader.fadeIn();
-//             // },
-//             success: function (msg) {
-//                 // Message was sent
-//                 if (msg == 'OK') {
-//                     sLoader.fadeOut();
-//                 }
-//                 // There was an error
-//                 else {
-//                     // sLoader.fadeOut();
-//                     // $('#message-warning').html(msg);
-//                     // $('#message-warning').fadeIn();
-//                 }
-//             },
-//             error: function () {
-//                 // sLoader.fadeOut();
-//                 // $('#message-warning').html("Something went wrong. Please try again.");
-//                 // $('#message-warning').fadeIn();
-//             }
-//         });
-//     }
-// });
+function ajaxReq(form) {
+    $.ajax({
+        type: "POST",
+        url: "validate",
+        data: $(form).serialize(),
+        beforeSend: function () {
+            show_hide_Loader(form)
+        },
+        success: function (msg) {
+            if (msg == 'OK') {
+                show_hide_Loader(form)
+            }
+            else {
+            }
+        },
+        error: function () {
+        }
+    });
+}
